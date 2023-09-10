b1eab2a2c2021d3416a9aa1ddde471c31ab279aa
lleviackermann <supritkumar30@gmail.com>
2023-09-11 01:46:06 +0530

    forces

--- others/LUCIFER_Number.cpp
+++ others/LUCIFER_Number.cpp
@@ -1,174 +1,51 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace std;
-using namespace __gnu_pbds;
 
-template <typename T> 
-using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-
-#define endl "\n"
-#define fo(i, n) for (i = 0; i < n; i++)
-#define Fo(i, k, n) for (i = k; k < n; k++)
-#define pb push_back
 #define ll long long
-#define mp make_pair
-#define ff first
-#define ss second
-#define all(x) x.begin(), x.end()
-#define clr(x) memset(x, 0, sizeof(x))
-#define sortall(x) sort(all(x))
-#define tr(it, arr) for (auto it = arr.begin(); it != arr.end(); it++)
-#define PI 3.1415926535897932384626
-#define suprit ios_base::sync_with_stdio(0); cout.tie(0); cin.tie(0);
-#define line cout << endl;
+using namespace std;
 
-typedef pair<int, int> pi;
-typedef pair<ll, ll> pl;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef vector<pi> vpi;
-typedef vector<pl> vpl;
-typedef vector<vi> vvi;
-typedef vector<vl> vvl;
-typedef map<ll, ll> ml;
-typedef map<string, ll> msl;
-typedef map<ll, string> mls;
-typedef unordered_map<ll, ll> uml;
-typedef unordered_map<string, ll> umsl;
-typedef unordered_map<ll, string> umls;
-typedef set<ll> sl;
-typedef set<pair<ll, ll>> spl;
-typedef ordered_set<ll> osl;
-typedef ordered_set<pair<ll, ll>> ospl;
-
-const ll mod = 1e9 + 7;
-
-bool comp2(pair<ll, ll> &arr, pair<ll, ll> &b)
-{
-    if (arr.first == b.first)
-        return arr.second < b.second;
-    return arr.first < b.first;
-};
-
-template <typename T> void read(T i, T n, vector<T> &arr) { for(T j = i; j < n; j++) cin >> arr[j]; }
-template <typename T> void read(vector<T> &arr) { for(auto &j : arr) cin>>j; }
-
-#ifndef ONLINE_JUDGE
-
-template <typename T, typename V> void print(set<pair<T, V>> &arr) { for(auto &it : arr) cout<<it.first<<" "<<it.second<<endl; line}
-template <typename T, typename V> void print(pair<T, V>& pa) { cout<<pa.first<<" "<<pa.second<<endl; }
-template <typename T> void print(T i, T last, vector<T> &arr) { for(T j = i; j < last; j++) cout<<arr[j]<<" "; line} 
-template <typename T> void print(T i, vector<T> &arr) { for(T j = i; j < arr.size(); j++) cout<<arr[j]<<" "; line} 
-template <typename T> void print(vector<T> &arr) { for(auto &i : arr) cout<<i<<" "; line}
-template <typename T, typename V> void print(unordered_map<T, V>& arr) { for(auto &it : arr) cout<<it.first<<" "<<it.second<<endl; line}
-template <typename T, typename V> void print(map<T, V>& arr) { for(auto &it : arr) cout<<it.first<<" "<<it.second<<endl;}
-template <typename T> void print(unordered_set<T> &arr) { for(auto &it : arr) cout<<it<<" "; line }
-template <typename T> void print(ordered_set<T> &arr) { for(auto &it : arr) cout<<it<<" "; line }
-template <typename T> void print(set<T> &arr) { for(auto &it : arr) cout<<it<<" "; line }
-template <typename T, typename... Args> void print(T t, Args... args) { cout << t << " "; print(args...); }
-template <typename T> void print(T t) { cout<<t<<"\n"; }
-#define debug(x) cout<<#x<<" "<<x<<endl;
-#define debug2(x, y) cout<<#x<<" "<<x<<" "<<#y<<" "<<y<<endl;
-#define debug3(x, y, z) cout<<#x<<" "<<x<<" "<<#y<<" "<<y<<" "<<#z<<" "<<z<<endl;
-#define debug4(x, y, z, a) cout<<#x<<" "<<x<<" "<<#y<<" "<<y<<" "<<#z<<" "<<z<<" "<<#a<<" "<<a<<endl;
-
-#else
-
-#define print(x) 
-#define debug(x)
-#define debug2(x, y)
-#define debug3(x, y, z)
-#define debug4(x, y, z, a)
-
-#endif
-
-ll dp[55][55][2][12]  ;
-
-bool isPrime(int n)
-{
-    // Check if n=1 or n=0
-    if (n <= 1)
-        return false;
-    // Check if n=2 or n=3
-    if (n == 2 || n == 3)
-        return true;
-    // Check whether n is divisible by 2 or 3
-    if (n % 2 == 0 || n % 3 == 0)
-        return false;
-     
-    // Check from 5 to square root of n
-    // Iterate i by (i+6)
-    for (int i = 5; i <= sqrt(n); i = i + 6)
-        if (n % i == 0 || n % (i + 2) == 0)
-            return false;
- 
-    return true;
-}
-
-ll raOneCities(string &s, int n, int check, int even, int odd) {
-    if(n == 0) {
-        if(isPrime(even-odd)) {
-            debug2(even, odd);
-            return 1;   
-        }
-        else return 0;
-    }
-
-    int answer = 0;
-    if(dp[even][odd][check][n] != -1) return dp[even][odd][check][n];
-
-    int ub = check ? (s[s.size() - n] - '0') : 9;
-    debug4(ub, n, even, odd);
-    for(int digit = 0; digit <= ub; digit++) {
-        int x = n % 2;
-        if(!x) even += digit;
-        else odd += digit;
-        answer += raOneCities(s, n-1, check && (digit == ub), even, odd);
-        if(!x) even -= digit;
-        else odd -= digit;
-    }
-
-    dp[even][odd][check][n] = answer;
-    return answer;
-}
 void solve()
 {
-    ll l, r;
-    cin>>l>>r;
-    l -= l > 0;
-    string left = to_string(l);
-    string right = to_string(r);
-    memset(dp, -1, sizeof dp);
-    int totalSum = raOneCities(right, right.size(), 1, 0, 0);
-    memset(dp, -1, sizeof dp);
-    debug(totalSum);
-    int useless = raOneCities(left, left.size(), 1, 0, 0);
-
-    cout<<totalSum - useless<<endl;
-
+    ll n;
+    cin >> n;
+    vector<ll> arr(n), val(n);
+    for(auto &i : arr) cin>>i;
+    for(auto &i : val) cin>>i;
+    for (int i = 1; i < n; i++)
+        val[i] += val[i - 1];
+    ll avg;
+    cin >> avg;
+    for (int i = 0; i < n; i++)
+        arr[i] -= avg;
+    for (int i = 1; i < n; i++)
+        arr[i] += arr[i - 1];
+    map<ll, ll> m;
+    vector<ll> dp(n, -1e9);
+    if (arr[0] == 0)
+    {
+        dp[0] = val[0];
+    }
+    m[arr[0]] = 1;
+    ll ans = 0;
+    ans = max(ans, dp[0]);
+    for (int i = 1; i < n; i++)
+    {
+        ll temp = m[arr[i]];
+        temp--;
+        if (temp != -1)
+        {
+            ll x = val[i] - val[temp];
+            dp[i] = max(dp[temp], 0ll) + x;
+        }
+        m[arr[i]] = i + 1;
+        ans = max(ans, dp[i]);
+    }
+    cout << ans << endl;
 }
 
-int main()
-{ 
-    suprit;
-    clock_t start = clock();
-
-    int t = 1;
-    cin >> t;
-    while (t--)
-    {
+int main() {
+    int t;
+    cin>>t;
+    while(t--) {
         solve();
     }
-    clock_t end = clock();
-    double elapsed = double(end - start) / CLOCKS_PER_SEC;
-    
-    #ifndef ONLINE_JUDGE
-    cout << setprecision(10) << elapsed << endl;
-    #endif
-    return 0;
 }
-
-
-
-
