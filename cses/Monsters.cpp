5436e66190a018c63305768191ed168597c6761b
lleviackermann <supritkumar30@gmail.com>
2023-09-18 15:27:38 +0530

    cses

---
+++ cses/Monsters.cpp
@@ -0,0 +1,277 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define endl "\n"
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n; k++)
+#define pb push_back
+#define ll long long
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, arr) for (auto it = arr.begin(); it != arr.end(); it++)
+#define PI 3.1415926535897932384626
+#define suprit                    \
+    ios_base::sync_with_stdio(0); \
+    cout.tie(0);                  \
+    cin.tie(0);
+#define line cout << endl;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef map<ll, ll> ml;
+typedef map<string, ll> msl;
+typedef map<ll, string> mls;
+typedef unordered_map<ll, ll> uml;
+typedef unordered_map<string, ll> umsl;
+typedef unordered_map<ll, string> umls;
+typedef set<ll> sl;
+typedef set<pair<ll, ll>> spl;
+typedef ordered_set<ll> osl;
+typedef ordered_set<pair<ll, ll>> ospl;
+
+const ll mod = 1e9 + 7;
+
+bool comp2(pair<ll, ll> &arr, pair<ll, ll> &b)
+{
+    if (arr.first == b.first)
+        return arr.second < b.second;
+    return arr.first < b.first;
+};
+
+template <typename T>
+void read(T i, T n, vector<T> &arr)
+{
+    for (T j = i; j < n; j++)
+        cin >> arr[j];
+}
+template <typename T>
+void read(vector<T> &arr)
+{
+    for (auto &j : arr)
+        cin >> j;
+}
+
+#ifndef ONLINE_JUDGE
+
+template <typename T, typename V>
+void print(set<pair<T, V>> &arr)
+{
+    for (auto &it : arr)
+        cout << it.first << " " << it.second << endl;
+    line
+}
+template <typename T, typename V>
+void print(pair<T, V> &pa) { cout << pa.first << " " << pa.second << endl; }
+template <typename T>
+void print(T i, T last, vector<T> &arr)
+{
+    for (T j = i; j < last; j++)
+        cout << arr[j] << " ";
+    line
+}
+template <typename T>
+void print(T i, vector<T> &arr)
+{
+    for (T j = i; j < arr.size(); j++)
+        cout << arr[j] << " ";
+    line
+}
+template <typename T>
+void print(vector<T> &arr)
+{
+    for (auto &i : arr)
+        cout << i << " ";
+    line
+}
+template <typename T, typename V>
+void print(unordered_map<T, V> &arr)
+{
+    for (auto &it : arr)
+        cout << it.first << " " << it.second << endl;
+    line
+}
+template <typename T, typename V>
+void print(map<T, V> &arr)
+{
+    for (auto &it : arr)
+        cout << it.first << " " << it.second << endl;
+}
+template <typename T>
+void print(unordered_set<T> &arr)
+{
+    for (auto &it : arr)
+        cout << it << " ";
+    line
+}
+template <typename T>
+void print(ordered_set<T> &arr)
+{
+    for (auto &it : arr)
+        cout << it << " ";
+    line
+}
+template <typename T>
+void print(set<T> &arr)
+{
+    for (auto &it : arr)
+        cout << it << " ";
+    line
+}
+template <typename T, typename... Args>
+void print(T t, Args... args)
+{
+    cout << t << " ";
+    print(args...);
+}
+template <typename T>
+void print(T t) { cout << t << "\n"; }
+#define debug(x) cout << #x << " " << x << endl;
+#define debug2(x, y) cout << #x << " " << x << " " << #y << " " << y << endl;
+#define debug3(x, y, z) cout << #x << " " << x << " " << #y << " " << y << " " << #z << " " << z << endl;
+#define debug4(x, y, z, a) cout << #x << " " << x << " " << #y << " " << y << " " << #z << " " << z << " " << #a << " " << a << endl;
+
+#else
+
+#define print(x)
+#define debug(x)
+#define debug2(x, y)
+#define debug3(x, y, z)
+#define debug4(x, y, z, a)
+
+#endif
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<string> arr(n);
+    read(arr);
+    int dx[] = {1, -1, 0, 0};
+    int dy[] = {0, 0, 1, -1};
+    vvi parent(n, vi(m, -1));
+    vvi monsters(n, vi(m, 1e9));
+    queue<pi> temp;
+    int startX, startY;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (arr[i][j] == 'M')
+                temp.push({i, j}), monsters[i][j] = 0;
+            else if (arr[i][j] == 'A')
+                startX = i, startY = j;
+        }
+    }
+    if (startX == 0 || startX == n - 1 || startY == 0 || startY == m - 1)
+    {
+        cout << "YES\n0\n";
+        return;
+    }
+    while (!temp.empty())
+    {
+        pi to = temp.front();
+        temp.pop();
+        int x = to.first, y = to.second;
+        for (int i = 0; i < 4; i++)
+        {
+            int newX = x + dx[i], newY = y + dy[i];
+            if (newX < 0 || newX >= n || newY < 0 || newY >= m || arr[newX][newY] == '#' || monsters[newX][newY] <= monsters[x][y] + 1)
+            {
+                continue;
+            }
+            temp.push({newX, newY});
+            monsters[newX][newY] = monsters[x][y] + 1;
+        }
+    }
+    // for (auto &i : monsters)
+    // {
+    //     for (auto j : i)
+    //         cout << j << " ";
+    //     cout << endl;
+    // }
+    vvi corr(n, vi(m, 1e9));
+    temp.push({startX, startY});
+    corr[startX][startY] = 0;
+    parent[startX][startY] = 0;
+    while (!temp.empty())
+    {
+        pi to = temp.front();
+        temp.pop();
+        int x = to.first, y = to.second;
+        for (int i = 0; i < 4; i++)
+        {
+            int newX = x + dx[i], newY = y + dy[i];
+            if (newX < 0 || newX >= n || newY < 0 || newY >= m || arr[newX][newY] == '#' || monsters[newX][newY] <= corr[x][y] + 1 || corr[newX][newY] != 1e9)
+            {
+                continue;
+            }
+            // cout<<i<<"  ";
+            temp.push({newX, newY});
+            corr[newX][newY] = corr[x][y] + 1;
+            parent[newX][newY] = i + 1;
+            if (newX == 0 || newX == n - 1 || newY == 0 || newY == m - 1)
+            {
+                int ano = parent[newX][newY];
+                string ans = "";
+                while(ano!=0) {
+                    if(ano==1) {
+                        ans += "D";
+                        newX--;
+                    } else if(ano==2) {
+                        ans += "U";
+                        newX++;
+                    } else if(ano==3) {
+                        ans += "R";
+                        newY--;
+                    } else {
+                        ans += "L";
+                        newY++;
+                    }
+                    ano = parent[newX][newY];
+                }
+                reverse(ans.begin(), ans.end());
+                cout<<"YES\n";
+                cout<<ans.size()<<endl;
+                cout<<ans<<endl;
+                return;
+            }
+        }
+    }
+    cout<<"NO\n";
+}
+
+int main()
+{
+    suprit;
+    clock_t start = clock();
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    clock_t end = clock();
+    double elapsed = double(end - start) / CLOCKS_PER_SEC;
+
+#ifndef ONLINE_JUDGE
+    cout << setprecision(10) << elapsed << endl;
+#endif
+    return 0;
+}
