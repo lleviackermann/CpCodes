89947c75c55b4d1d07e8c98a5f50bbdd7b1f9698
lleviackermann <supritkumar30@gmail.com>
2024-06-18 18:15:49 +0530

    codeforces

---
+++ codeforces/D_Fixing_a_Binary_String.cpp
@@ -0,0 +1,176 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+template <typename T> 
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define endl "\n"
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n; k++)
+#define pb push_back
+#define ll long long
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, arr) for (auto it = arr.begin(); it != arr.end(); it++)
+#define PI 3.1415926535897932384626
+#define suprit ios_base::sync_with_stdio(0); cout.tie(0); cin.tie(0);
+#define line cout << endl;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef map<ll, ll> ml;
+typedef map<string, ll> msl;
+typedef map<ll, string> mls;
+typedef unordered_map<ll, ll> uml;
+typedef unordered_map<string, ll> umsl;
+typedef unordered_map<ll, string> umls;
+typedef set<ll> sl;
+typedef set<pair<ll, ll>> spl;
+typedef ordered_set<ll> osl;
+typedef ordered_set<pair<ll, ll>> ospl;
+
+const ll mod = 1e9 + 7;
+
+bool comp2(pair<ll, ll> &arr, pair<ll, ll> &b)
+{
+    if (arr.first == b.first)
+        return arr.second < b.second;
+    return arr.first < b.first;
+};
+
+template <typename T> void read(T i, T n, vector<T> &arr) { for(T j = i; j < n; j++) cin >> arr[j]; }
+template <typename T> void read(vector<T> &arr) { for(auto &j : arr) cin>>j; }
+
+#ifndef ONLINE_JUDGE
+
+template <typename T, typename V> void print(set<pair<T, V>> &arr) { for(auto &it : arr) cout<<it.first<<" "<<it.second<<endl; line}
+template <typename T, typename V> void print(pair<T, V>& pa) { cout<<pa.first<<" "<<pa.second<<endl; }
+template <typename T> void print(T i, T last, vector<T> &arr) { for(T j = i; j < last; j++) cout<<arr[j]<<" "; line} 
+template <typename T> void print(T i, vector<T> &arr) { for(T j = i; j < arr.size(); j++) cout<<arr[j]<<" "; line} 
+template <typename T> void print(vector<T> &arr) { for(auto &i : arr) cout<<i<<" "; line}
+template <typename T, typename V> void print(unordered_map<T, V>& arr) { for(auto &it : arr) cout<<it.first<<" "<<it.second<<endl; line}
+template <typename T, typename V> void print(map<T, V>& arr) { for(auto &it : arr) cout<<it.first<<" "<<it.second<<endl;}
+template <typename T> void print(unordered_set<T> &arr) { for(auto &it : arr) cout<<it<<" "; line }
+template <typename T> void print(ordered_set<T> &arr) { for(auto &it : arr) cout<<it<<" "; line }
+template <typename T> void print(set<T> &arr) { for(auto &it : arr) cout<<it<<" "; line }
+template <typename T, typename... Args> void print(T t, Args... args) { cout << t << " "; print(args...); }
+template <typename T> void print(T t) { cout<<t<<"\n"; }
+#define debug(x) cout<<#x<<" "<<x<<endl;
+#define debug2(x, y) cout<<#x<<" "<<x<<" "<<#y<<" "<<y<<endl;
+#define debug3(x, y, z) cout<<#x<<" "<<x<<" "<<#y<<" "<<y<<" "<<#z<<" "<<z<<endl;
+#define debug4(x, y, z, a) cout<<#x<<" "<<x<<" "<<#y<<" "<<y<<" "<<#z<<" "<<z<<" "<<#a<<" "<<a<<endl;
+
+#else
+
+#define print(x) 
+#define debug(x)
+#define debug2(x, y)
+#define debug3(x, y, z)
+#define debug4(x, y, z, a)
+
+#endif
+
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+    vi arr;
+    int cnt = 1;
+    for(int i = 1; i < n; i++) {
+        if(s[i] != s[i-1]) arr.push_back(cnt), cnt = 1;
+        else cnt++;
+    }
+    arr.push_back(cnt);
+    print(arr);
+    int te = arr.size();
+    vi prefix(te, 0), suffix(te, 0);
+    vi sum(te, 0);
+    prefix[0] = (arr[0] == k ? 1 : 0);
+    sum[0] = arr[0];
+    for(int i = 1; i < te; i++) {
+        prefix[i] = prefix[i-1] & (arr[i] == k);
+        sum[i] = sum[i-1] + arr[i];
+    }
+    if(prefix.back()) {
+        cout << s.size() << endl;
+        return;
+    }
+    print(prefix);
+    suffix[te-1] = (arr.back() == k ? 1 : 0);
+    for(int i = te -2; i >= 0; i--) {
+        if(i == te - 2) suffix[i] = (arr[i] == k);
+        else suffix[i] = suffix[i+1] & (arr[i] == k);
+    }
+    print(suffix);
+    int par = ((te-1) & 1);
+    for(int i = 0; i < te; i++) {
+        if(arr[i] == k) continue;
+        if(arr[i] > 2*k) {
+            cout << "-1\n";
+            return;
+        }
+        int cur_par = (i & 1);
+        if(par != cur_par) {
+            if(arr[i] == 2*k) {
+                if(arr.back() == k) {
+                    bool flag = (i < te - 2 ? suffix[i+1] : 1);
+                    flag = flag & (i > 0 ? prefix[i-1] : 1);
+                    if(flag) {
+                        debug(i);
+                        int ans = (i ? sum[i-1] : 0);
+                        ans += k;
+                        cout << ans << endl;
+                        return;
+                    }
+                }
+            }
+        }
+        if(par != cur_par || arr[i] == 2*k || arr.back() + (arr[i] % k) != k) continue;
+        bool flag = (i < te - 2 ? suffix[i+1] : 1);
+        flag = flag & (i > 0 ? prefix[i-1] : 1);
+        if(flag) {
+            debug2(i, flag);
+            int ans = (i ? sum[i-1] : 0);
+            ans += (arr[i] % k);
+            cout << ans << endl;
+            return;
+        }
+    }
+    cout << "-1\n";
+}
+
+int main()
+{ 
+    suprit;
+    clock_t start = clock();
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    clock_t end = clock();
+    
+    #ifndef ONLINE_JUDGE
+    double elapsed = double(end - start) / CLOCKS_PER_SEC;
+    cout << setprecision(10) << elapsed << endl;
+    #endif
+    return 0;
+}
