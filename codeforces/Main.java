cbae0791b7cfcc047f9263b23d8340168bec92d1
lleviackermann <supritkumar30@gmail.com>
2024-08-31 02:21:03 +0530

    practice

---
+++ codeforces/Main.java
@@ -0,0 +1,14 @@
+class Hello{
+	public Hello(){
+		System.out.println("Hello");
+	}
+}
+
+public class Main{
+
+	
+	public static void main(String[] args) {
+		Hello hello = new Hello();
+		System.out.println("Hello World!");
+	}
+}
