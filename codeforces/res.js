f4a0b17d532c3c3bc018621ffe633d46a3bc097a
lleviackermann <supritkumar30@gmail.com>
2025-01-03 16:25:45 +0530

    practice

--- codeforces/res.js
+++ codeforces/res.js
@@ -1,40 +1,25 @@
-function findPlatform(landings, takeoffs, totalFlights) {
-    function sortSchedule(schedule) {
-        return [...schedule].sort((a, b) => a - b);
-    }
+function first() {
+    var cnt = 10
+    function second() {
+        let cnt = 121
+        function third() {
+            cnt++;
 
-    let sortedLandings = [...landings]
-    let sortedTakeoffs = [...takeoffs]
-    
-    for(let i = 0; i < totalFlights; i++) {
-        if(sortedTakeoffs[i] < sortedLandings[i]) sortedTakeoffs[i] = 2359;
-    }
-
-    sortedLandings = sortSchedule(sortedLandings);
-    sortedTakeoffs = sortSchedule(sortedTakeoffs);
-
-    let maxStations = 0;
-    let activeStations = 0;
-    console.log(sortedLandings);
-    console.log(sortedTakeoffs);
-    
-    
-    // Use for loop instead of while
-    for (let i = 0, j = 0; i < totalFlights || j < totalFlights;) {
-        if (i < totalFlights && (j >= totalFlights || sortedLandings[i] <= sortedTakeoffs[j])) {
-            activeStations++;
-            i++;
-        } else {
-            activeStations--;
-            j++;
+            console.log(cnt)
         }
 
-        maxStations = Math.max(maxStations, activeStations);
+        return third;
+
     }
+    cnt = 25
+    return second();
+}
 
-    return maxStations;
-}   
+const func = first();
+func()
+func()
+func()
+func()
+func()
+func()
 
-const landings = [2200, 2300]
-const takeoffs = [200, 300]
-console.log(findPlatform(landings, takeoffs, 2));
